531ddf3dd68b80de3c7e31633a77f3d549f67126
gabioxs03 <gabrielsamaniego2016@gmail.com>
2025-08-06 16:55:50 -0300

    se agregaron metodos por extension como AddDomainServices y DbContextExtensions; se agrego Authentication al program y se configuro JWT Bearer

---
+++ TaskFlow.Api/Configurations/DomainServicesExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.Application.Interfaces;
+using TaskFlow.Application.Services;
+using TaskFlow.Data;
+using TaskFlow.Data.Repositories;
+using TaskFlow.Domain.Interfaces;
+
+namespace TaskFlow.Api.Configurations;
+
+public static class DomainServicesConfigurationExtension
+{
+    public static IServiceCollection AddDomainServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<TaskFlowContext>(options =>
+        {
+
+            services.AddDbContext<TaskFlowContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("TaskFlowEntities")));
+            /*options.UseSeeding((c, t) =>
+                {
+                    ((Dsw2025Ej15Context)c).Seedwork<Category>("Sources\\categories.json");
+                    ((Dsw2025Ej15Context)c).Seedwork<SubCategory>("Sources\\sub-categories.json");
+                    ((Dsw2025Ej15Context)c).Seedwork<Product>("Sources\\products.json");
+                });*/
+        });
+        services.AddScoped<IRepository, EfRepository>();
+        services.AddTransient<ITasksManagementService, TasksManagementService>();
+        services.AddTransient<IUsersManagementService, UsersManagementService>();
+
+        return services;
+    }
+}
--- TaskFlow.Application/obj/Debug/net8.0/TaskFlow.Application.AssemblyInfo.cs
+++ TaskFlow.Application/obj/Debug/net8.0/TaskFlow.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b88e321918236549a5c4d02d1bbca08f76fbd6e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
